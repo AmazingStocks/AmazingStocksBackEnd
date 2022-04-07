2c3c2ddb662510783586770444557329c36b9eb8
Arnab Senapati <senapati.arnab@gmail.com>
2022-04-07 17:19:21 +0530

    cognito integration

--- application.py
+++ application.py
@@ -1,13 +1,28 @@
 from flask import Flask
 from yahoo_fin import stock_info as si
+from flask_awscognito import AWSCognitoAuthentication
+import os
+
 
 application = Flask(__name__)
+
+application.config['AWS_DEFAULT_REGION'] = os.environ['AWS_DEFAULT_REGION']
+application.config['AWS_COGNITO_DOMAIN'] = os.environ['AWS_COGNITO_DOMAIN']
+application.config['AWS_COGNITO_USER_POOL_ID'] = os.environ['AWS_COGNITO_USER_POOL_ID']
+application.config['AWS_COGNITO_USER_POOL_CLIENT_ID'] = os.environ['AWS_COGNITO_USER_POOL_CLIENT_ID']
+application.config['AWS_COGNITO_USER_POOL_CLIENT_SECRET'] = os.environ['AWS_COGNITO_USER_POOL_CLIENT_SECRET']
+application.config['AWS_COGNITO_REDIRECT_URL'] = os.environ['AWS_COGNITO_REDIRECT_URL']
+
+aws_auth = AWSCognitoAuthentication(application)
+
+
 
 @application.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @application.route("/getallSymbols", methods=["GET"])
+@aws_auth.authentication_required
 def getAllSymbols():
     lst = si.tickers_nifty50()
     ret = dict()
